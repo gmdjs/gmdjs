14c906934c2649cbd39e329c8d17b108f678a12c
GriNAME <griname@gmail.com>
2016-11-13 16:29:34 +0300

    add: mixin `+align-#()` upd: documentation and some files minor bug fixes

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,11 @@
-var browserSync   = require('browser-sync'),
+let browserSync   = require('browser-sync'),
 		gulp          = require('gulp'),
-		autoprefixer  = require('gulp-autoprefixer'),
-		cleanCss      = require('gulp-clean-css'),
-		gcmq          = require('gulp-group-css-media-queries'),
-		image         = require('gulp-image'),
+		postcss       = require('gulp-postcss'),
+		autoprefixer  = require('autoprefixer'),
+		cssnano       = require('cssnano'),
+		media         = require('gulp-group-css-media-queries'),
 		sass          = require('gulp-sass'),
-		notify        = require('gulp-notify'),
-		bourbon       = require('node-bourbon'),
-		uglify        = require('gulp-uglify');
+		bourbon       = require('node-bourbon');
 		
 gulp.task('browserSync', function () {
 	browserSync({
@@ -18,20 +16,23 @@ gulp.task('browserSync', function () {
 	})
 });
 
-gulp.task('sass', function () {
-	return gulp.src('assets/sass/*.sass')
-			.pipe(sass({
-				includePaths: bourbon.includePaths
-			}).on("error", notify.onError()))
-			.pipe(autoprefixer(['last 10 versions']))
-			.pipe(gcmq())
-			.pipe(cleanCss())
-			.pipe(gulp.dest('assets/css'))
+gulp.task('postcss', function () {
+	let processor = ([
+			//autoprefixer({browsers: ['last 10 version']}),
+			cssnano()
+	]);
+	return gulp.src('./assets/sass/*.sass')
+			.pipe(sass({includePaths: bourbon.includePaths}))
+			.pipe(media())
+			.pipe(postcss(processor))
+			.pipe(gulp.dest('./assets/css'))
 			.pipe(browserSync.reload({stream: true}))
 });
 
-gulp.task('watch', ['sass', 'browserSync'], function () {
-	gulp.watch('assets/sass/**/*.sass', ['sass']);
+
+
+gulp.task('watch', ['postcss', 'browserSync'], function () {
+	gulp.watch('assets/sass/**/*.sass', ['postcss']);
 	gulp.watch('assets/*.html', browserSync.reload);
 	gulp.watch('assets/js/**/*.js', browserSync.reload);
 });
